c21470578c9cfe4314231e7320661e34733b47b3
Victor Untulius <untulius@gmail.com>
2020-03-16 21:59:14 +0300

    Add SimpleTest

---
+++ src/test/java/com/junit5/SimpleTest.java
@@ -0,0 +1,38 @@
+package com.junit5;
+
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleTest.class);
+
+    @BeforeAll
+    static void beforeAll(){
+        LOG.info("Before All");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        LOG.info("Before Each");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        LOG.info("After All");
+    }
+
+    @AfterEach
+    void afterEach(){
+        LOG.info("After Each");
+    }
+    @Test
+    void simpleTest1(){
+        LOG.info("Test1 complete");
+    }
+
+    @Test
+    void simpleTest2(){
+        LOG.info("Test2 complete");
+    }
+}
