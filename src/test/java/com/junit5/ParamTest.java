a485a46623caf62db78154131dae749ec5a57239
Victor Untulius <untulius@gmail.com>
2020-03-18 15:26:59 +0300

    Add junit-jupiter-params dependency

--- src/test/java/com/junit5/ParamTest.java
+++ src/test/java/com/junit5/ParamTest.java
@@ -1,12 +1,15 @@
 package com.junit5;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ParamTest {
     private static final Logger LOG = LoggerFactory.getLogger(ParamTest.class);
 
-    private Object[][] dataProvider(){
+
+    private static Object[][] dataProvider(){
         return new Object[][] {
                 {"Vasily", 46},
                 {"Ivan", 35},
@@ -14,6 +17,8 @@ public class ParamTest {
         };
     }
 
+    @ParameterizedTest
+    @MethodSource("dataProvider")
     public void paramTest1(String name, int age){
         String output = String.format("Name: '%s' | Age: '%d'", name, age);
         LOG.info(output);
